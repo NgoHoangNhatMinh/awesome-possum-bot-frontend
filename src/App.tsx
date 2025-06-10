b3f0937748f9d30a4a234db0aae5cfda3ada42fd
Minh <ngohoangnhatminh@gmail.com>
2025-06-10 23:25:07 +0800

    feat: add make best move responses

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,52 @@ import { useState } from "react";
 import "./App.css";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
+import axios from "axios";
 
 function App() {
   const [game, setGame] = useState(new Chess());
 
   function makeAMove(move: { from: string; to: string; promotion?: string }) {
     const gameCopy = new Chess(game.fen());
+    console.log("Attempting move:", move);
     const result = gameCopy.move(move);
     if (result === null) return null; // illegal move
     setGame(gameCopy);
     return gameCopy;
   }
 
-  function makeRandomMove(currentGame: Chess) {
-    const possibleMoves = currentGame.moves({ verbose: true });
-    if (currentGame.isGameOver() || possibleMoves.length === 0) return;
-    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    const gameCopy = new Chess(currentGame.fen());
-    gameCopy.move(possibleMoves[randomIndex]);
-    setGame(gameCopy);
+  // function makeRandomMove(currentgame: Chess) {
+  //   const possiblemoves = currentgame.moves({ verbose: true });
+  //   if (currentgame.isGameOver() || possiblemoves.length === 0) return;
+  //   const randomindex = Math.floor(Math.random() * possiblemoves.length);
+  //   const gamecopy = new Chess(currentgame.fen());
+  //   const move = possiblemoves[randomindex];
+  //   console.log("Random move chosen:", move);
+  //   gamecopy.move(move);
+  //   setGame(gamecopy);
+  // }
+
+  function makeBestMove(currentGame: Chess) {
+    const fen = currentGame.fen();
+    const bestMove = axios
+      .post("http://localhost:8080/best", { fen })
+      .then((response) => {
+        const move = {
+          from: response.data.fromString,
+          to: response.data.toString,
+          // promotion: "q",
+        };
+        console.log("Best move received:", move);
+        if (move) {
+          const gameCopy = new Chess(currentGame.fen());
+          gameCopy.move(move);
+          setGame(gameCopy);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching best move:", error);
+      });
+    return bestMove;
   }
 
   function onDrop(sourceSquare: string, targetSquare: string) {
@@ -31,8 +58,14 @@ function App() {
     });
 
     if (updatedGame === null) return false; // illegal move
-    setTimeout(() => makeRandomMove(updatedGame), 200); // make a random move after a short delay
+    // setTimeout(() => makeRandomMove(updatedGame), 200); // make a random move after a short delay
+    // setTimeout(() => makeBestMove(updatedGame), 200); // make a random move after a short delay
+    makeBestMove(updatedGame);
     return true;
+  }
+
+  function handleRestartGame() {
+    setGame(new Chess());
   }
 
   return (
@@ -44,6 +77,8 @@ function App() {
           autoPromoteToQueen={true}
         />
       </div>
+      <br />
+      <button onClick={handleRestartGame}>Restart game</button>
     </>
   );
 }
